fbdeb642818c7878c648ae5f3fe574334f3bac09
Moustafa_Ashraf <moustafa.ashraf277@gmail.com>
2024-08-27 09:43:16 +0300

    feat: add about us & services page

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../../shared/components/header/header.component";
-import { FooterComponent } from "../../shared/components/footer/footer.component";
+import { HeaderComponent } from '../../shared/components/header/header.component';
+import { FooterComponent } from '../../shared/components/footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, RouterModule],
   templateUrl: './services.component.html',
-  styleUrl: './services.component.css'
+  styleUrl: './services.component.css',
 })
-export class ServicesComponent {
-
-}
+export class ServicesComponent {}
